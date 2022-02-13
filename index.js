ff8505ae3a1baee0e8890f284bc7b19e50063b01
Roman <54202786+Panhead32@users.noreply.github.com>
2022-02-13 22:32:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+﻿const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+const PORT = 3000;
+
+const corsOptions = {
+  origin: '*',
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors());
+
+app.use('/js', express.static(path.join(__dirname,'js')));
+app.use('/css',express.static(path.join(__dirname,'css')));
+app.use('/assets', express.static(path.join(__dirname,'assets')));
+app.use('/fonts', express.static(path.join(__dirname,'fonts')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.get('/thanks', (req, res) => {
+  res.sendFile(path.join(__dirname, 'thanks.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+})
---
+++ js/main2.js
@@ -0,0 +1,2 @@
+﻿const userName = localStorage.getItem('name')
+document.querySelector('#thanks').innerHTML = (userName || '___') + document.querySelector('#thanks').innerHTML;
